bfc05aff737ee7f711a68ae4441f036f2b71dcb4
BDJLSJ <nmsl@menglolita.com>
2026-07-30 02:16:41 +0800

    v7.7.2.9303: Buff栏增强 & 卡牌叠加 & 连点修复 & 事件日志修复 & 更新日志精简

--- service-worker.js
+++ service-worker.js
@@ -1,5 +1,5 @@
-// Service Worker - Bunny CC v7.7.2.9302
-const CACHE_VERSION = 'v7.7.2.9302';
+// Service Worker - Bunny CC v7.7.2.9303
+const CACHE_VERSION = 'v7.7.2.9303';
 const CACHE_NAME = `bunny-cc-${CACHE_VERSION}`;
 const RUNTIME_CACHE = `bunny-cc-runtime-${CACHE_VERSION}`;
 const CORE_ASSETS = [
