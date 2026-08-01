3a3936afd61e2d387e8932cce3af8d30e35d1ab6
BDJLSJ <nmsl@menglolita.com>
2026-08-01 17:11:12 +0800

    feat: bcos system control commands + theme-adaptive button (v7.8.1.9328)

--- service-worker.js
+++ service-worker.js
@@ -1,5 +1,5 @@
-// Service Worker - Bunny CC v7.8.0.9327
-const CACHE_VERSION = 'v7.8.0.9327';
+// Service Worker - Bunny CC v7.8.1.9328
+const CACHE_VERSION = 'v7.8.1.9328';
 const CACHE_NAME = `bunny-cc-${CACHE_VERSION}`;
 const RUNTIME_CACHE = `bunny-cc-runtime-${CACHE_VERSION}`;
 const CORE_ASSETS = [
@@ -20,6 +20,7 @@ const OLD_CACHE_PATTERNS = [
     'bunny-cc-v7.8.0.9324', 'bunny-cc-runtime-v7.8.0.9324',
     'bunny-cc-v7.8.0.9325', 'bunny-cc-runtime-v7.8.0.9325',
     'bunny-cc-v7.8.0.9326', 'bunny-cc-runtime-v7.8.0.9326',
+    'bunny-cc-v7.8.0.9327', 'bunny-cc-runtime-v7.8.0.9327',
     'bunny-cc-v7.7.2.9312', 'bunny-cc-runtime-v7.7.2.9312',
     'bunny-cc-v7.7.2.9305', 'bunny-cc-runtime-v7.7.2.9305',
     'bunny-cc-v7.7.2.9306', 'bunny-cc-runtime-v7.7.2.9306',
