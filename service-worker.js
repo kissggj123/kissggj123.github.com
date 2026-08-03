632b16e08172d5eb50d849ad1c8e313bb513ea12
BDJLSJ <nmsl@menglolita.com>
2026-08-03 23:22:34 +0800

    Fix null owner property error in executeAutoPilot and upgrade AutoPilot profit strategy v7.8.1.9362

--- service-worker.js
+++ service-worker.js
@@ -1,5 +1,5 @@
-// Service Worker - Bunny CC v7.8.1.9361
-const CACHE_VERSION = 'v7.8.1.9361';
+// Service Worker - Bunny CC v7.8.1.9362
+const CACHE_VERSION = 'v7.8.1.9362';
 const CACHE_NAME = `bunny-cc-${CACHE_VERSION}`;
 const RUNTIME_CACHE = `bunny-cc-runtime-${CACHE_VERSION}`;
 const CORE_ASSETS = [
@@ -13,6 +13,7 @@ const CORE_ASSETS = [
 
 // Old cache versions to force-purge (ensures icon refresh)
 const OLD_CACHE_PATTERNS = [
+    'bunny-cc-v7.8.1.9361', 'bunny-cc-runtime-v7.8.1.9361',
     'bunny-cc-v7.8.1.9360', 'bunny-cc-runtime-v7.8.1.9360',
     'bunny-cc-v7.8.1.9359', 'bunny-cc-runtime-v7.8.1.9359',
     'bunny-cc-v7.8.1.9358', 'bunny-cc-runtime-v7.8.1.9358',
